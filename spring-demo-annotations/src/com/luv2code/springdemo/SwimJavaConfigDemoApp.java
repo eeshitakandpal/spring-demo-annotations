d8a698f335e537491ab5a575cc42effb73c58f01
Eeshita <eeshitakandpal@gmail.com>
2021-08-05 18:55:21 +0530

    edit

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -0,0 +1,33 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SwimJavaConfigDemoApp {
+
+	public static void main(String[] args) {
+				
+		
+				//read spring config java class
+				AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+				
+				//get the bean from spring container
+				//Coach theCoach = context.getBean("swimCoach", Coach.class);
+				SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
+				
+				//call our new swim coach methods.. that has props values injecteed
+				System.out.println("Email: "+theCoach.getEmail());
+				System.out.println("Team: "+theCoach.getTeam());
+				
+				//call a method on the bean
+				//System.out.println(theCoach.getDailyWorkout());
+				
+				// call method to get daily fortune
+				//System.out.println(theCoach.getDailyFortune());
+				
+				//close the context
+				context.close();
+
+	}
+
+}
